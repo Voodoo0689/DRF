1f09827527b2c7f813f2bfdac7d967ceb945e465
Aremiy <voodoo06.89@mail.ru>
2021-11-20 18:44:13 +0300

    completed lesson 2

---
+++ todo/frontend/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import axios from "axios";
+import {render} from "@testing-library/react";
+import UserList from "./components/Users";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users':[]
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users/').then(
+            response =>{
+                const users = response.data
+                this.setState(
+                    {
+                        'users':users
+                    }
+                )
+            }
+        ).catch(error => console.log(error))
+
+    }
+
+
+    render()
+    {
+
+        return (
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+
+
+
+        );
+    };
+}
+
+export default App;
