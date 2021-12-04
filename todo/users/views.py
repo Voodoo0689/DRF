af5530c38754fb3747c8cc9c6b4cac1c736e3ed4
Aremiy <voodoo06.89@mail.ru>
2021-12-04 23:08:20 +0300

    Lesson6

--- todo/users/views.py
+++ todo/users/views.py
@@ -9,38 +9,44 @@ from rest_framework.viewsets import ViewSet, ModelViewSet
 from .models import Users
 from .serializers import UsersModelSerializer
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 
 
-# class UsersModelViewSet(ModelViewSet):
-#     # renderer_classes = [JSONRenderer]
+class StaffOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_staff
+
+
+class UsersModelViewSet(ModelViewSet):
+    # renderer_classes = [JSONRenderer]
+    # permission_classes = [StaffOnly]
+    queryset = Users.objects.all()
+    serializer_class = UsersModelSerializer
+
+# class UsersModelViewSet(ViewSet):
+#     permission_classes = [StaffOnly]
+#     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
 #     queryset = Users.objects.all()
-#     serializer_class = UsersModelSerializer
-
-
-class UsersModelViewSet(ViewSet):
-    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
-
-    def update(self, request, pk=None, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        user = get_object_or_404(Users, pk=pk)
-        serializer = UsersModelSerializer(user, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        if getattr(user, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            user._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
-
-    def list(self, request):
-        users = Users.objects.all()
-        serializer = UsersModelSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        user = get_object_or_404(Users, pk=pk)
-        serializer = UsersModelSerializer(user)
-        return Response(serializer.data)
+#     def update(self, request, pk=None, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         user = get_object_or_404(Users, pk=pk)
+#         serializer = UsersModelSerializer(user, data=request.data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#
+#         if getattr(user, '_prefetched_objects_cache', None):
+#             # If 'prefetch_related' has been applied to a queryset, we need to
+#             # forcibly invalidate the prefetch cache on the instance.
+#             user._prefetched_objects_cache = {}
+#
+#         return Response(serializer.data)
+#
+#     def list(self, request):
+#         users = Users.objects.all()
+#         serializer = UsersModelSerializer(users, many=True)
+#         return Response(serializer.data)
+#
+#     def retrieve(self, request, pk=None):
+#         user = get_object_or_404(Users, pk=pk)
+#         serializer = UsersModelSerializer(user)
+#         return Response(serializer.data)
